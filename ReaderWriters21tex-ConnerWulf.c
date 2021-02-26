6584f7cc66aaa89001932d544fc99ee27ab2bed0
Conner <connerwulf@usf.edu>
2021-02-26 13:38:02 -0500

    Initial Run

--- ReaderWriters21tex-ConnerWulf.c
+++ ReaderWriters21tex-ConnerWulf.c
@@ -11,20 +11,6 @@
 
 /* Student: Conner Wulf. */
 
-/* Time Comparison:
- When comparing the mutex and semaphore solutions to our programs critical section
- We see that both solutions run in the same overall time, roughly 2 seconds.
-
- Over half of the mutex solutions runtime is taken up by User time, or time the CPU
- spent executing the user program. Whereas  most of the the semaphore solution runtime
- is taken up by System time or the time the CPU spent executing system calls on behave of the process
-
- The reason for this is because in the mutex solution the busy waiting happens within the my code.
- The while loop, looping till it aquires the lock, is where the busy waiting happens.
-
- In the semaphore solution the busy waiting section happens in the system calls wait(&semaphore).
- This is why the overall runtime is the same, but the user and system statistics are different.
- */
 
 //Global Variables
 struct rusage mytiming;
@@ -45,31 +31,15 @@ int jumps=0;
 * This function increases the value of shared variable "counter"
   by one 2750000 times, and by 100 when count-> is a multiple of 100
 ****************************************************************/
-void * thread1(void *arg)
+void * reader_thread(void *arg)
 {
-	int line = 0;
-  int temp = 0;
-	while (line < 2750000)
-	{
-
-    temp = jumps;
-    line++;
-    sem_wait(&semaphore);
-      counter->value = counter->value + 1;
-	    counter->value = counter->value * 2;
-	    counter->value = counter->value / 2;
-
-
-      if(counter->value % 100 == 0 && counter->value <= 5500000 - 100 && line <= 2750000 - 100)
-      {
-        counter->value = counter->value + 100;
-        line += 100;
-        jumps++;
-      }
-      sem_post(&semaphore);
-
+	int i;
+  int value;
+  for(i = 0; i < 250000000)
+  {
+    value = counter->value;
   }
-	printf("from process1 counter  =  %d, jumps %d \n", counter->value, jumps);
+	printf("Reader %d has finished", getpid());
   return(NULL);
 }
 
@@ -78,22 +48,15 @@ void * thread1(void *arg)
 This function increases the value of shared variable "counter"
 by one 275000 times
 ****************************************************************/
-void * thread2(void *arg)
+void * writer_thread(void *arg)
 {
 	int line = 0;
 
-	while (line < 2750000)
+	while (line < 25000)
 	{
-
-
-        line++;
-        sem_wait(&semaphore);
-        /* Critical Section */
-	       counter->value = counter->value + 1;
-	       counter->value = counter->value * 2;
-	       counter->value = counter->value / 2;
-         sem_post(&semaphore);
-
+    line++;
+    /* Critical Section */
+	  counter->value = counter->value + 1;
 
    }
 	   printf("from process2 counter = %d\n", counter->value);
@@ -101,75 +64,62 @@ return(NULL);
 }
 
 
-void * thread3(void *arg)
-{
-	int line = 0;
-
-	while (line < 2750000)
-	{
-
-
-        line++;
-        sem_wait(&semaphore);
-        /* Critical Section */
-	       counter->value = counter->value + 1;
-	       counter->value = counter->value * 2;
-	       counter->value = counter->value / 2;
-         sem_post(&semaphore);
-
-
-   }
-	   printf("from process3 counter = %d\n", counter->value);
-return(NULL);
-}
 
 /****************************************************************
 *                  Main Body                                    *
 ****************************************************************/
 int main()
 {
-  int r=0;
-	int i;
-  pthread_t	tid1[1];     /* process id for thread 1 */
-  pthread_t	tid2[1];     /* process id for thread 2 */
-  pthread_t	tid3[1];     /* process id for thread 2 */
-  pthread_attr_t	attr[1];     /* attribute pointer array */
+  int numOfReaders;
+  printf("How many readers should there be?\n");
+  scanf("%d", numOfReaders);
+  for(int i = 0; i<10;i++)
+  {
 
-	sem_init(&semaphore,0,1);
-  counter = (struct shared_dat *) malloc(sizeof(struct shared_dat));
-
-	/* initialize shared memory to 0 */
-	counter->value = 0;
-  printf("1 - I am here %d in pid %d\n",r,getpid());
-
-	fflush(stdout);
- /* Required to schedule thread independently.
- Otherwise use NULL in place of attr. */
+    printf("%d",numOfReaders);
+  }
+/*
+  pthread_t readers[numOfReaders];
+  pthread_t writer[1];
+  pthread_attr_t	attr[1];
+  int k = (int) (numOfReaders/2);
+*/
+/* Required to schedule thread independently.
+ Otherwise use NULL in place of attr.
   pthread_attr_init(&attr[0]);
-  pthread_attr_setscope(&attr[0], PTHREAD_SCOPE_SYSTEM);  /* system-wide contention */
+  pthread_attr_setscope(&attr[0], PTHREAD_SCOPE_SYSTEM);
 
-	/* end to schedule thread independently */
+for(int i = 0; i < k; i++)
+{
+  pthread_create(&readers[i], &attr[0], reader_thread, (void*) i);
+}
+*/
+/* Create the writer thread
+pthread_create(&writer[0], &attr[0], writer_thread, NULL);
 
-/* Create the threads */
+for(i = k ; i < numOfReaders ; i++)
+{
+  pthread_create(&readers[i], &attr[0], reader_thread, (void*) i);
+}
 
-  pthread_create(&tid2[0], &attr[0], thread2, NULL);
-  pthread_create(&tid3[0], &attr[0], thread3, NULL);
-  pthread_create(&tid1[0], &attr[0], thread1, NULL);
+for(int j = 0; j < numOfReaders; j++)
+{
+  if(j = 0)
+  {
+    pthread_join(writer[j], NULL);
 
+  }
+  pthread_join(readers[j], NULL);
+}
 
-/* Wait for the threads to finish */
-  pthread_join(tid2[0], NULL);
-  pthread_join(tid3[0], NULL);
-  pthread_join(tid1[0], NULL);
+*/
 
-
-
-	printf("from parent counter  =  %d\n", counter->value);
-  getrusage(RUSAGE_SELF, &mytiming);
-  printf("Time used is sec: %d, usec %d\n",mytiming.ru_utime.tv_sec,mytiming.ru_utime.tv_usec);
-  printf("System Time used is sec: %d, usec %d\n",mytiming.ru_stime.tv_sec,mytiming.ru_stime.tv_usec);
-	printf("---------------------------------------------------------------------------\n");
-	printf("\t\t	End of simulation\n");
+	// printf("from parent counter  =  %d\n", counter->value);
+  // getrusage(RUSAGE_SELF, &mytiming);
+  // printf("Time used is sec: %d, usec %d\n",mytiming.ru_utime.tv_sec,mytiming.ru_utime.tv_usec);
+  // printf("System Time used is sec: %d, usec %d\n",mytiming.ru_stime.tv_sec,mytiming.ru_stime.tv_usec);
+	// printf("---------------------------------------------------------------------------\n");
+	// printf("\t\t	End of simulation\n");
 
 	exit(0);
 
