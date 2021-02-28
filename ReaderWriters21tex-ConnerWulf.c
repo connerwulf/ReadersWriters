cf510f6c15d05d0073be7f3c80c5f398c7569290
Conner <connerwulf@usf.edu>
2021-02-27 22:25:42 -0500

    asd

--- ReaderWriters21tex-ConnerWulf.c
+++ ReaderWriters21tex-ConnerWulf.c
@@ -144,10 +144,10 @@ int main()
   {
     if(j = 0)
     {
-      printf("WriterJoin\n")
+      printf("WriterJoin\n");
       pthread_join(writer[j], NULL);
     }
-    printf("ReaderJoin\n")
+    printf("ReaderJoin\n");
     pthread_join(readers[j], NULL);
   }
 
